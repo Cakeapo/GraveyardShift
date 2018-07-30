f02c2c22e325b6bb5268b4580e5b0b4289e900a3
Unknown <jmannas@live.com>
2018-07-30 18:16:16 +1200

    First
    ye

---
+++ Graveyard Shift/Assets/Scripts/HeadstoneSpawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class HeadstoneSpawn : MonoBehaviour {
+
+    public Object[] headstones;
+    private Transform headstoneSpawn;
+
+	// Use this for initialization
+	void Start ()
+    {
+        headstoneSpawn = transform.GetChild(0).transform;
+
+        headstones = Resources.LoadAll("HeadStones", typeof(GameObject));
+
+        Instantiate(headstones[Random.Range(0, headstones.Length)], headstoneSpawn.position, headstoneSpawn.rotation);
+	}
+	
+}
