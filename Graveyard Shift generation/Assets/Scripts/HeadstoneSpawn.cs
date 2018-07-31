9263cc9c40b7cf7e53c04f815de7aed61e135715
Unknown <jmannas@live.com>
2018-07-31 19:10:54 +1200

    unity files

---
+++ Graveyard Shift generation/Assets/Scripts/HeadstoneSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class HeadstoneSpawn : MonoBehaviour {
+
+    public Object[] headstones;
+    private Transform headstoneSpawn;
+    private GameObject hs;
+
+	// Use this for initialization
+	void Start ()
+    {
+        headstoneSpawn = transform.GetChild(0).transform;
+
+        headstones = Resources.LoadAll("HeadStones", typeof(GameObject));
+
+        hs = Instantiate(headstones[Random.Range(0, headstones.Length)], headstoneSpawn.position, headstoneSpawn.rotation) as GameObject;
+
+        hs.transform.parent = this.gameObject.transform;
+        
+	}
+	
+}
