a0e93aaa2650fd2c538140ca2b80f7f6a3dfde19
Unknown <jmannas@live.com>
2018-08-06 19:53:11 +1200

    Generation scene update
    hedges!

---
+++ Graveyard Shift/Assets/HedgeSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HedgeSpawner : MonoBehaviour {
+
+    public GameObject hedgeBit;
+
+	// Use this for initialization
+	void Start ()
+    {
+		hedgeBit = Resources.Load<GameObject>("Hedges/Hedge_Straight");
+
+        int chance = Random.Range(0, 100);
+
+        if(chance < 25)
+        {
+            Instantiate(hedgeBit, transform.position, transform.rotation);
+        }
+    }
+	
+}
